7381569e27030f8f189300b1d0c1c571bdc94c19
Jorn Bostelaar <bostelaarj@gmail.com>
2018-05-09 23:41:31 +0200

    Make Header position fixed

--- src/app/components/modules/Header/styled.js
+++ src/app/components/modules/Header/styled.js
@@ -7,6 +7,11 @@ export const HeaderContainer = styled.header`
   justify-content: center;
   height: 120px;
   margin-bottom: 20px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  left: 0;
+  box-shadow: 0 2px 16px rgba(0,0,0,.02);
 `;
 
 export const Logo = styled.div`
--- src/app/components/modules/List/styled.js
+++ src/app/components/modules/List/styled.js
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 
 export const ListContainer = styled.section`
   max-width: 600px;
-  margin: 0 auto;
+  margin: 140px auto 0;
+  padding: 0 0 50px;
 `;
