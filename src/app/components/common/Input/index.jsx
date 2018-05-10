f9f1ada3da6008de1a652d1ff67948c234553190
Jorn Bostelaar <bostelaarj@gmail.com>
2018-05-10 21:16:16 +0200

    Improve Input with focus animation and icon

--- src/app/components/common/Input/index.jsx
+++ src/app/components/common/Input/index.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 import PT from 'prop-types';
 import styled from 'styled-components';
 
+import AddIcon from 'vectors/icon-add.svg';
+
+const InputContainer = styled.div`
+  position: relative;
+  svg {
+    position: absolute;
+    left: 0px;
+    top: 19px;
+    width: 15px;
+    height: 15px;
+    fill: ${props => props.theme.dodgerBlue};
+    opacity: 0;
+  }
+
+  &:focus-within {
+    svg {
+      left: 20px;
+      opacity: 1;
+      transition: all .1s ease;
+    }
+  }
+`;
+
 const StyledInput = styled.input`
   -webkit-appearance: none;
   -moz-appearance: none;
@@ -11,17 +34,19 @@ const StyledInput = styled.input`
   font-size: 16px;
   border-radius: 4px;
   color: ${props => props.theme.navyBlue};
-  background-color: ${props => props.theme.white};
   border: solid 1px ${props => props.theme.borderColor};
   display: block;
   padding: 0 20px;
   font-family: 'Rubik', sans-serif;
   outline: none;
   color: ${props => props.theme.sherpaBlue};
+  background: ${props => props.theme.paleGrey};
 
   &:focus {
     border: 1px solid ${props => props.theme.dodgerBlue};
     box-shadow: 0px 2px 8px rgba(25, 181, 254,.3);
+    padding: 0 20px 0 50px;
+    transition: all .1s ease;
   }
 
   &::placeholder {
@@ -30,13 +55,16 @@ const StyledInput = styled.input`
 `;
 
 const Input = ({ input, placeholder, className }) => (
-  <StyledInput
-    {...input}
-    placeholder={placeholder}
-    className={className}
-    autoComplete="off"
-    tabIndex="1"
-  />
+  <InputContainer>
+    <AddIcon />
+    <StyledInput
+      {...input}
+      placeholder={placeholder}
+      className={className}
+      autoComplete="off"
+      tabIndex="1"
+    />
+  </InputContainer>
 );
 
 Input.propTypes = {
